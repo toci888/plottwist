876db236b9cd384bacbd7931021dca4822c97ebf
toci888 <bzapart@gmail.com>
2024-10-30 06:59:13 +0100

    synchro

--- Toci.PlotTwist.Vsix/Toci.PlotTwist.Vsix/Toci.PlotTwist.VsixPackage.cs
+++ Toci.PlotTwist.Vsix/Toci.PlotTwist.Vsix/Toci.PlotTwist.VsixPackage.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿using ChatGptExtension;
+using Microsoft.VisualStudio.Shell;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -46,6 +47,10 @@ namespace Toci.PlotTwist.Vsix
         // When initialized asynchronously, the current thread may be a background thread at this point.
         // Do any initialization that requires the UI thread after switching to the UI thread.
         await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            ChatGptWindow chat = new ChatGptWindow();
+
+            //chat.InitializeComponent();
     }
 
     #endregion
